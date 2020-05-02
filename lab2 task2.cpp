d9829d1874d749456f1af2e9c4ed7afd1bdcff55
Krivosh <64680810+Krivosh@users.noreply.github.com>
2020-05-02 22:39:28 +0300

    Update lab2 task2.cpp

--- lab2 task2.cpp
+++ lab2 task2.cpp
@@ -2,24 +2,32 @@
 #include <string.h>
 using namespace std;
 char* InvertStr(char* s, int k, int n) {
-static char ts[40];
-int i = 0, j = k + n;
-if (k > strlen(s))
-return 0;
-while (k > 0 && k > j) {
-ts[i] = s[k - 1];
-i++;
-k--;
-}
-return ts;
+	static char ts[256];
+	int i = 0, j = k + n;
+	if (k > strlen(s)) {
+		system("pause");
+		return 0;
+	}
+	
+	while (k >0 && k < j) {
+		ts[i] = s[k-1 ];
+		i++;
+		k--;
+		
+	}
+	return ts;
 }
 int main() {
-char s[228];
-int k, n;
-cin >> s;
-cin >> k;
-cin >> n;
-cout << InvertStr(s, k, n) << endl;
-system("pause");
-return 0;
+	setlocale(LC_ALL, "Russian");
+	char s[228];
+	int k, n;
+	cout << "Введите строку s : ";
+	cin >> s;
+	cout << "Введите номер символа с котрого  будет инвертироваться строка s : ";
+	cin >> k;
+	cout << "Введите количество символо для инвертирования : ";
+	cin >> n;
+	cout << InvertStr(s, k, n) << endl;
+	system("pause");
+	return 0;
 }
